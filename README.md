f90d6ad900daaa103f3d904ab3ada6b81025a94a
BlackPing <47684708+BlackPing@users.noreply.github.com>
2019-10-21 22:50:47 +0900

    3강

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # node
 Git Connection
+
+Node 강의 Sample Code
+https://nodeman.tistory.com/category/NodeJS
+
+## NodeJS 0강 : 소개
+## NodeJS 1강 : 개발환경 구성
+## NodeJS 2강 : express 시작하기
+## NodeJS 3강 : babel, nodemon
--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('hi nodeman')
+console.time('Server ON Time2');
+import express from 'express';
+const app = express();
+
+class component {
+    constructor(port, ip, route) {
+        this.port = port;
+        this.ip = ip;
+        this.route = route;
+    }
+
+    routing(app) {
+        for(var i = 0; i < this.route.path.length; i++) {
+            app.get(this.route.path[i], function(req, res) {
+                res.send('nodeman');
+            });
+        }
+    }
+
+    listen(app) {
+        app.listen(this.port, this.ip, () => {
+            console.timeEnd('Server ON Time2');
+        });
+    }
+}
+
+const server = new component(3000, '127.0.0.1', { path: ['/', '/node'] });
+
+server.routing(app);
+server.listen(app);
